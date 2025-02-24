c05958a3e1525dd7e81f95ed13e9c8b97c1ff03e
Stefan Schuhart <stefan.schuhart@gv.hamburg.de>
2025-02-24 09:25:22 +0000

    chore: src and project template setup

---
+++ docs/content/about/project_description.md
@@ -0,0 +1,5 @@
+# The noiseProcesses project
+
+Wrap up what this project is about.
+
+A python wrapper for NoiseModelling
---
+++ src/noiseprocesses/__init__.py
@@ -0,0 +1,8 @@
+"""noiseProcesses package.
+
+A python wrapper for NoiseModelling
+"""
+
+from __future__ import annotations
+
+__all__: list[str] = []
