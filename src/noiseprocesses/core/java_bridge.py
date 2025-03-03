82c5ef6815861e485a4bbac3198c3a7c88d0d660
Stefan Schuhart <stefan.schuhart@gv.hamburg.de>
2025-03-03 09:39:45 +0000

    rewritten the java_bridge to use jpype instead of pyjnius

--- src/noiseprocesses/core/java_bridge.py
+++ src/noiseprocesses/core/java_bridge.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from typing import Optional
-
-import jnius_config
+import jpype
+import jpype.imports
 
 
 class JavaBridge:
@@ -19,11 +19,14 @@ class JavaBridge:
         current_dir = Path(__file__).parent
         lib_dir = (current_dir.parent.parent.parent / "dist" / "lib").resolve()
 
-        # Configure JVM
-        jnius_config.add_options("-Xmx4096m")
-        classpath = str(lib_dir / "*")
-        jnius_config.set_classpath(classpath)
-
+        # Configure and start JVM if not already running
+        if not jpype.isJVMStarted():
+            classpath = str(lib_dir / "*")
+            jpype.startJVM(jpype.getDefaultJVMPath(), 
+                          "-Xmx4096m", 
+                          f"-Djava.class.path={classpath}",
+                          convertStrings=True)
+        
         # Initialize commonly used classes
         self._init_classes()
 
@@ -33,55 +36,50 @@ class JavaBridge:
             cls._instance = JavaBridge()
         return cls._instance
 
+    @classmethod
+    def shutdown(cls):
+        if jpype.isJVMStarted():
+            jpype.shutdownJVM()
+        cls._instance = None
+        
     def _init_classes(self):
         """Initialize commonly used Java classes."""
-
-        # importing late, because it initializes the jvm!
-        from jnius import autoclass
-
-        # Core classes
-        self.File = autoclass("java.io.File")
-        self.DBUtils = autoclass("org.h2gis.utilities.dbtypes.DBUtils")
-        self.LDENConfig = autoclass("org.noise_planet.noisemodelling.jdbc.LDENConfig")
-        self.LDENConfig_INPUT_MODE = autoclass(
-            "org.noise_planet.noisemodelling.jdbc.LDENConfig$INPUT_MODE"
-        )
-        self.LDENConfig_TIME_PERIOD = autoclass(
-            "org.noise_planet.noisemodelling.jdbc.LDENConfig$TIME_PERIOD"
-        )
-        self.LDENPropagationProcessData = autoclass(
-            "org.noise_planet.noisemodelling.jdbc.LDENPropagationProcessData"
-        )
-        self.PropagationProcessPathData = autoclass(
-            "org.noise_planet.noisemodelling.propagation.PropagationProcessPathData"
-        )
-
-        # Database related
-        self.ConnectionWrapper = autoclass(
-            "org.h2gis.utilities.wrapper.ConnectionWrapper"
-        )
-        self.GeoJsonDriverFunction = autoclass(
-            "org.h2gis.functions.io.geojson.GeoJsonDriverFunction"
-        )
-        self.TableLocation = autoclass("org.h2gis.utilities.TableLocation")
-
-        # Utility classes
-        self.EmptyProgressVisitor = autoclass("org.h2gis.api.EmptyProgressVisitor")
-        self.PowerUtils = autoclass(
-            "org.noise_planet.noisemodelling.pathfinder.utils.PowerUtils"
-        )
-
-        self.DriverManager = autoclass("java.sql.DriverManager")
-        self.H2GISFunctions = autoclass("org.h2gis.functions.factory.H2GISFunctions")
-        self.Properties = autoclass("java.util.Properties")
-
-        self.GeometryTableUtilities = autoclass(
-            "org.h2gis.utilities.GeometryTableUtilities"
-        )
-
-        self.JDBCUtilities = autoclass("org.h2gis.utilities.JDBCUtilities")
-
-        # Add spatial result set classes
-        self.SpatialResultSet = autoclass("org.h2gis.utilities.SpatialResultSet")
-        # Add spatial result set classes
-        self.SpatialResultSet = autoclass("org.h2gis.utilities.SpatialResultSet")
+        # Using JPype's import style
+        from java.io import File # type: ignore
+        from java.sql import DriverManager
+        from java.util import Properties
+        
+        from org.h2gis.utilities.dbtypes import DBUtils
+        from org.h2gis.utilities.wrapper import ConnectionWrapper
+        from org.h2gis.utilities import TableLocation, GeometryTableUtilities, JDBCUtilities, SpatialResultSet
+        from org.h2gis.functions.io.geojson import GeoJsonDriverFunction
+        from org.h2gis.functions.factory import H2GISFunctions
+        from org.h2gis.api import EmptyProgressVisitor
+        
+        from org.noise_planet.noisemodelling.jdbc import LDENConfig, LDENPropagationProcessData
+        from org.noise_planet.noisemodelling.propagation import PropagationProcessPathData
+        from org.noise_planet.noisemodelling.pathfinder.utils import PowerUtils
+        
+        # Store classes as instance attributes
+        self.File = File
+        self.DBUtils = DBUtils
+        self.LDENConfig = LDENConfig
+        self.LDENConfig_INPUT_MODE = LDENConfig.INPUT_MODE
+        self.LDENConfig_TIME_PERIOD = LDENConfig.TIME_PERIOD
+        self.LDENPropagationProcessData = LDENPropagationProcessData
+        self.PropagationProcessPathData = PropagationProcessPathData
+        
+        self.ConnectionWrapper = ConnectionWrapper
+        self.GeoJsonDriverFunction = GeoJsonDriverFunction
+        self.TableLocation = TableLocation
+        
+        self.EmptyProgressVisitor = EmptyProgressVisitor
+        self.PowerUtils = PowerUtils
+        
+        self.DriverManager = DriverManager
+        self.H2GISFunctions = H2GISFunctions
+        self.Properties = Properties
+        
+        self.GeometryTableUtilities = GeometryTableUtilities
+        self.JDBCUtilities = JDBCUtilities
+        self.SpatialResultSet = SpatialResultSet
