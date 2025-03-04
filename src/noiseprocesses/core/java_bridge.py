3c4bffc5bc137d5b6af6aa3b8c1185d085f31daa
Stefan Schuhart <stefan.schuhart@gv.hamburg.de>
2025-03-04 13:16:03 +0000

    some formatting, added PointNoiseMap

--- src/noiseprocesses/core/java_bridge.py
+++ src/noiseprocesses/core/java_bridge.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from typing import Optional
+
 import jpype
 import jpype.imports
 
@@ -25,29 +26,24 @@ class JavaBridge:
             jpype.startJVM(
                 jpype.getDefaultJVMPath(),
                 # Memory configuration
-                "-Xmx4096m",           # Maximum heap size
-                "-Xms1024m",           # Initial heap size
-                
+                "-Xmx4096m",  # Maximum heap size
+                "-Xms1024m",  # Initial heap size
                 # GC optimization
-                "-XX:+UseG1GC",        # Use G1 garbage collector
+                "-XX:+UseG1GC",  # Use G1 garbage collector
                 "-XX:MaxGCPauseMillis=200",  # Target max GC pause time
-                
                 # Database specific options
                 "-Dh2.serverCachedObjects=3000",  # H2 object cache size
                 "-Dh2.objectCacheMaxPerElementSize=4096",  # Max size per object
                 "-Dh2.bigDecimalIsDecimal=true",  # Improved decimal handling
-                
                 # String optimization (important for GIS)
                 "-XX:+OptimizeStringConcat",
-                
                 # Class path
                 f"-Djava.class.path={classpath}",
-                
                 # JPype options
-                convertStrings=True,    # Auto convert strings
-                interrupt=True         # Allow thread interruption
+                convertStrings=True,  # Auto convert strings
+                interrupt=True,  # Allow thread interruption
             )
-        
+
         # Initialize commonly used classes
         self._init_classes()
 
@@ -62,40 +58,46 @@ class JavaBridge:
         if jpype.isJVMStarted():
             jpype.shutdownJVM()
         cls._instance = None
-        
+
     def _init_classes(self):
         """Initialize commonly used Java classes."""
         # Using JPype's import style
-        from java.io import File # type: ignore
-        from java.sql import DriverManager # type: ignore
-        from java.util import Properties # type: ignore
-        
-        from org.h2gis.utilities.dbtypes import DBUtils # type: ignore
-        from org.h2gis.utilities.wrapper import ConnectionWrapper # type: ignore
-        from org.h2gis.utilities import ( # type: ignore
-            TableLocation, GeometryTableUtilities,
-            JDBCUtilities, SpatialResultSet
+        from java.io import File  # type: ignore
+        from java.sql import DriverManager  # type: ignore
+        from java.util import Properties  # type: ignore
+        from org.h2gis.api import EmptyProgressVisitor  # type: ignore
+        from org.h2gis.functions.factory import H2GISFunctions  # type: ignore
+        from org.h2gis.functions.io.asc import AscReaderDriver  # type: ignore
+        from org.h2gis.functions.io.geojson import GeoJsonDriverFunction  # type: ignore
+        from org.h2gis.functions.io.utility import PRJUtil  # type: ignore
+        from org.h2gis.functions.spatial.crs import (  # type: ignore
+            ST_SetSRID,
+            ST_Transform,
         )
-        from org.h2gis.functions.io.geojson import GeoJsonDriverFunction # type: ignore
-        from org.h2gis.functions.factory import H2GISFunctions # type: ignore
-        from org.h2gis.api import EmptyProgressVisitor # type: ignore
-        
-        from org.noise_planet.noisemodelling.jdbc import ( # type: ignore
-            LDENConfig, LDENPropagationProcessData
+        from org.h2gis.utilities import (  # type: ignore
+            GeometryTableUtilities,
+            JDBCUtilities,
+            SpatialResultSet,
+            TableLocation,
         )
-        from org.noise_planet.noisemodelling.propagation import ( # type: ignore
-            PropagationProcessPathData
+        from org.h2gis.utilities.dbtypes import DBUtils  # type: ignore
+        from org.h2gis.utilities.wrapper import ConnectionWrapper  # type: ignore
+        from org.locationtech.jts.io import WKTReader, WKTWriter  # type: ignore
+        from org.noise_planet.noisemodelling.jdbc import (  # type: ignore
+            LDENConfig,
+            LDENPropagationProcessData,
+            PointNoiseMap
         )
-        from org.noise_planet.noisemodelling.pathfinder.utils import ( # type: ignore
-            PowerUtils
+        from org.noise_planet.noisemodelling.pathfinder import ( # type: ignore
+            RootProgressVisitor,
+        )
+        from org.noise_planet.noisemodelling.pathfinder.utils import (  # type: ignore
+            PowerUtils,
+        )
+        from org.noise_planet.noisemodelling.propagation import (  # type: ignore
+            PropagationProcessPathData,
         )
 
-        from org.h2gis.functions.io.asc import AscReaderDriver # type: ignore
-        from org.h2gis.functions.spatial.crs import ST_SetSRID, ST_Transform # type: ignore
-        from org.h2gis.functions.io.utility import PRJUtil # type: ignore
-        from org.locationtech.jts.io import WKTReader, WKTWriter # type: ignore
-        from org.noise_planet.noisemodelling.pathfinder import RootProgressVisitor # type: ignore
-        
         # Store classes as instance attributes
         self.File = File
         self.DBUtils = DBUtils
@@ -104,18 +106,19 @@ class JavaBridge:
         self.LDENConfig_TIME_PERIOD = LDENConfig.TIME_PERIOD
         self.LDENPropagationProcessData = LDENPropagationProcessData
         self.PropagationProcessPathData = PropagationProcessPathData
-        
+        self.PointNoiseMap = PointNoiseMap
+
         self.ConnectionWrapper = ConnectionWrapper
         self.GeoJsonDriverFunction = GeoJsonDriverFunction
         self.TableLocation = TableLocation
-        
+
         self.EmptyProgressVisitor = EmptyProgressVisitor
         self.PowerUtils = PowerUtils
-        
+
         self.DriverManager = DriverManager
         self.H2GISFunctions = H2GISFunctions
         self.Properties = Properties
-        
+
         self.GeometryTableUtilities = GeometryTableUtilities
         self.JDBCUtilities = JDBCUtilities
         self.SpatialResultSet = SpatialResultSet
