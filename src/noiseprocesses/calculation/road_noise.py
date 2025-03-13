6a78a9bc8dfc524732517aa03ec37cea19ef146b
Stefan Schuhart <stefan.schuhart@gv.hamburg.de>
2025-03-13 08:10:16 +0000

    first part of noise calculation procedure added

--- src/noiseprocesses/calculation/road_noise.py
+++ src/noiseprocesses/calculation/road_noise.py
@@ -1,30 +1,51 @@
+from typing import Any, Dict
+from noiseprocesses.calculation.road_propagation import RoadPropagationCalculator
+from noiseprocesses.models.grid_config import DelaunayGridConfig
+from noiseprocesses.models.internal import BuildingsFeatureCollectionInternal, RoadsFeatureCollectionInternal
+from noiseprocesses.models.noise_calculation_config import NoiseCalculationConfig
+from noiseprocesses.models.user_input import PropagationUserInput
 from noiseprocesses.protocols import (
-    EmissionCalculator, PropagationCalculator, GridGenerator
+    PropagationCalculator, GridGenerator
+
 )
+from noiseprocesses.utils.grids import DelaunayGridGenerator
 from noiseprocesses.core.database import NoiseDatabase
+
+from fastprocesses.processes.process_registry import register_process
+from fastprocesses.core.base_process import BaseProcess
+from fastprocesses.core.models import (
+    ProcessDescription,
+    ProcessInput,
+    ProcessJobControlOptions,
+    ProcessOutput,
+    ProcessOutputTransmission,
+    Schema,
+)
+
+@register_process("simple_process")
+class TrafficNoiseProcess(BaseProcess):
+    async def execute(self, inputs: Dict[str, Any]) -> Dict[str, Any]:
+        pass
+
 
 class RoadNoiseModellingCalculator:
     """Main class handling the complete noise calculation process"""
     
     def __init__(
         self,
-        emission_calculator: EmissionCalculator,
-        propagation_calculator: PropagationCalculator,
-        grid_generator: GridGenerator,
-        database_name: str
+        noise_calculation_config: NoiseCalculationConfig | None = None
     ):
-        self.emission_calculator = emission_calculator
-        self.propagation_calculator = propagation_calculator
-        self.grid_generator = grid_generator
-        self.database = NoiseDatabase(database_name)
+        self.config = noise_calculation_config or NoiseCalculationConfig()
+        
+        self.database = NoiseDatabase(
+            db_file=self.config.database.name,
+            in_memory=self.config.database.in_memory
+        )
 
     def calculate_noise_levels(
         self,
-        source_table: str,
-        receivers_table: str,
-        buildings_table: str,
-        **kwargs
-    ) -> str:
+        user_input: PropagationUserInput
+    ) -> dict:
         """
         Calculate complete noise levels including emission and propagation
         
@@ -37,15 +58,55 @@ class RoadNoiseModellingCalculator:
         Returns:
             str: Name of final results table
         """
-        # Calculate emissions first
-        emission_table = self.emission_calculator.calculate_emissions(source_table)
-        
-        # Use emission results for propagation
-        result_table = self.propagation_calculator.calculate_propagation(
-            emission_table=emission_table,
-            receivers_table=receivers_table,
-            buildings_table=buildings_table,
-            **kwargs
+        # validate user inputs
+            # - buildings user input -> buildings internal
+        buildings = BuildingsFeatureCollectionInternal.from_user_collection(
+            user_input.buildings
         )
+            # - roads user input -> roads internal
+        roads_traffic = RoadsFeatureCollectionInternal.from_user_collection(
+            user_input.roads
+        )
+            # - ...
+
+        # setup the database
+        noise_db = NoiseDatabase(
+            self.config.database.name,
+            self.config.database.in_memory
+        )
+
+        # import data
+            # - buildings -> geojson
+        noise_db.import_geojson(
+            buildings.model_dump(exclude_none=True), # omit empty fields like bbox
+            self.config.required_tables.building_table
+        )
+            # - roads -> geojson
+        noise_db.import_geojson(
+            roads_traffic.model_dump(exclude_unset=True), # omit empty fields like bbox
+            self.config.required_tables.roads_table
+        )
+            # - load dem -> tif
+            # - grounds -> geojson
+
+        # generate receivers (using Delaunay with triangle creation)
+        grid_config = DelaunayGridConfig(
+            buildings_table=self.config.required_tables.building_table,
+            output_table=self.config.required_tables.receivers_table,
+            height=2.75,  # 4 meters receiver height
+            sources_table=self.config.required_tables.roads_table,
+            road_width=15
+        )
+
+        delauny_generator = DelaunayGridGenerator(noise_db)
+        delauny_generator.generate_receivers(grid_config)
+
+        # calculate propagation
+        road_prop = RoadPropagationCalculator(noise_db)
+        road_prop.calculate_propagation(self.config)
+
+        # create isocontour
+
+        # export to dict/geojson/FeatureCollection
         
-        return result_table
+        return {}
