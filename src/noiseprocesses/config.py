6074b9c1d7999abd33db50e0ef4cd4a93b24f184
Stefan Schuhart <stefan.schuhart@gv.hamburg.de>
2025-05-28 13:20:48 +0000

    renamed settings

--- src/noiseprocesses/config.py
+++ src/noiseprocesses/config.py
@@ -1,19 +1,20 @@
-from pydantic_settings import BaseSettings
 from logging import getLogger
+
+from pydantic_settings import BaseSettings
 
 logger = getLogger(__name__)
 
+
 class AppConfig(BaseSettings):
-    JAVA_LIB_DIR: str | None = None
-    LOG_LEVEL: str = "INFO"
-    JAVA_MAX_HEAP_SIZE: str = "4096m"  # Maximum heap size for the JVM
-    JAVA_INITIAL_HEAP_SIZE: str = "1024m"  # Initial heap size for the JVM
+    NP_JAVA_LIB_DIR: str | None = None
+    NP_LOG_LEVEL: str = "INFO"
+    NP_JAVA_MAX_HEAP_SIZE: str = "4096m"  # Maximum heap size for the JVM
+    NP_JAVA_INITIAL_HEAP_SIZE: str = "1024m"  # Initial heap size for the JVM
 
     def print_settings(self):
-        logger.info(
-            f"Current {self.__class__.__name__} settings:"
-        )
+        logger.info(f"Current {self.__class__.__name__} settings:")
         logger.info(vars(self))
+
 
 config = AppConfig()
 
