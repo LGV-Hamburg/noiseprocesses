3a43670817dbc444ca9a5582e6be5d8c31f01590
Stefan Schuhart <stefan.schuhart@gv.hamburg.de>
2025-07-01 13:01:44 +0000

    added database in_memory option

--- src/noiseprocesses/config.py
+++ src/noiseprocesses/config.py
@@ -10,6 +10,7 @@ class AppConfig(BaseSettings):
     NP_LOG_LEVEL: str = "INFO"
     NP_JAVA_MAX_HEAP_SIZE: str = "4096m"  # Maximum heap size for the JVM
     NP_JAVA_INITIAL_HEAP_SIZE: str = "1024m"  # Initial heap size for the JVM
+    NP_DATABASE_IN_MEMORY: bool = False  # Use in-memory database
 
     def print_settings(self):
         logger.info(f"Current {self.__class__.__name__} settings:")
