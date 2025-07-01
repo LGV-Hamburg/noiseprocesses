0eebc5f74c614afb5ebf818ca86312935d2cfab2
Stefan Schuhart <stefan.schuhart@gv.hamburg.de>
2025-07-01 14:05:24 +0000

    fixed: redundant database setup

--- src/noiseprocesses/calculation/road_building_immissions.py
+++ src/noiseprocesses/calculation/road_building_immissions.py
@@ -222,11 +222,6 @@ class ImmissionsAroundBuildingsCalculator:
         if dem_url:
             dem_path = load_convert_save_dem(dem_url)
 
-        # setup the database
-        noise_db = NoiseDatabase(
-            self.config.database.name, self.config.database.in_memory
-        )
-
         if progress_callback:
             progress_callback(3, "Importing into database")
 
@@ -237,14 +232,14 @@ class ImmissionsAroundBuildingsCalculator:
 
         # import data
         # - buildings -> geojson import
-        noise_db.import_geojson(
+        self.database.import_geojson(
             buildings.model_dump(exclude_none=True),  # omit empty fields like bbox
             self.config.required_input.building_table,
             crs,
         )
 
         # - roads -> geojson import
-        noise_db.import_geojson(
+        self.database.import_geojson(
             roads_traffic.model_dump(exclude_unset=True),  # omit empty fields like bbox
             self.config.required_input.roads_table,
             crs,
@@ -256,7 +251,7 @@ class ImmissionsAroundBuildingsCalculator:
 
         # - load dem -> tif
         if dem_path:
-            noise_db.import_raster(
+            self.database.import_raster(
                 dem_path,
                 self.config.optional_input.dem_table,
                 int(crs),
@@ -264,7 +259,7 @@ class ImmissionsAroundBuildingsCalculator:
 
         # - grounds -> geojson
         if grounds:
-            noise_db.import_geojson(
+            self.database.import_geojson(
                 grounds.model_dump(exclude_none=True),
                 self.config.optional_input.ground_absorption_table,
                 crs,
@@ -284,7 +279,7 @@ class ImmissionsAroundBuildingsCalculator:
             )
         # - translate user input to config
         # - default: 2D
-        grid_generator = BuildingGridGenerator2d(noise_db)
+        grid_generator = BuildingGridGenerator2d(self.database)
 
         grid_config = BuildingGridConfig(
             buildings_table=self.config.required_input.building_table,
@@ -298,7 +293,7 @@ class ImmissionsAroundBuildingsCalculator:
         has_stack_id = False
 
         if user_input.building_grid_settings.grid_type == GridType.BUILDINGS_3D:
-            grid_generator = BuildingGridGenerator3d(noise_db)
+            grid_generator = BuildingGridGenerator3d(self.database)
             has_stack_id = True
 
         grid_generator.generate_receivers(grid_config)
@@ -309,7 +304,7 @@ class ImmissionsAroundBuildingsCalculator:
             )
 
         # calculate propagation
-        road_prop = RoadPropagationCalculator(noise_db)
+        road_prop = RoadPropagationCalculator(self.database)
         road_prop.calculate_propagation(
             self.config,
             True if dem_url else False,
@@ -331,7 +326,7 @@ class ImmissionsAroundBuildingsCalculator:
 
             # export to dict/geojson/FeatureCollection
             # H2 DB has no support for in-memory data export
-            surface_file = noise_db.export_data(table_name)
+            surface_file = self.database.export_data(table_name)
 
             # read the data...
             # replace path with actual data
