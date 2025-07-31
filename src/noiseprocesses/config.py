afee47d61dd18f3b75724e315380fda919392b74
Stefan Schuhart <stefan.schuhart@gv.hamburg.de>
2025-07-31 09:58:37 +0000

    java heap to integers

--- src/noiseprocesses/config.py
+++ src/noiseprocesses/config.py
@@ -8,8 +8,8 @@ logger = getLogger(__name__)
 class AppConfig(BaseSettings):
     NP_JAVA_LIB_DIR: str | None = None
     NP_LOG_LEVEL: str = "INFO"
-    NP_JAVA_MAX_HEAP_SIZE: str = "4096m"  # Maximum heap size for the JVM
-    NP_JAVA_INITIAL_HEAP_SIZE: str = "1024m"  # Initial heap size for the JVM
+    NP_JAVA_MAX_HEAP_SIZE: int = 4096  # Maximum heap size for the JVM
+    NP_JAVA_INITIAL_HEAP_SIZE: int = 1024  # Initial heap size for the JVM
     NP_DATABASE_IN_MEMORY: bool = False  # Use in-memory database
 
     def print_settings(self):
